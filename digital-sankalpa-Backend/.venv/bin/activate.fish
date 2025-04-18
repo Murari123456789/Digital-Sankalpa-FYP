72b14544d471e8dcfe0a92f173dfe65ef9310407
Murari123456789 <blaze123795@gmail.com>
2025-04-18 17:59:14 +0545

    setup changes

--- digital-sankalpa-Backend/.venv/bin/activate.fish
+++ digital-sankalpa-Backend/.venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /Users/nischallamichhane/Desktop/work/digital-sankalpa/.venv
+set -gx VIRTUAL_ENV /Users/muraripathak/Desktop/Digital-Sankalpa-FYP/digital-sankalpa-Backend/.venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
