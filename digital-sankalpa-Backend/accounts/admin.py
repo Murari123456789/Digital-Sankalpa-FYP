fed5be6adae3663a0d7b7979b4d4ada8f220ac4d
Murari123456789 <blaze123795@gmail.com>
2025-04-21 12:00:00 +0545

    Add backend updates: admin and views logic

--- digital-sankalpa-Backend/accounts/admin.py
+++ digital-sankalpa-Backend/accounts/admin.py
@@ -1,4 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
-admin.site.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    # Only show selected fields in the admin form
+    fieldsets = (
+        (None, {'fields': ('username', 'first_name', 'last_name', 'email', 'is_active', 'date_joined', 'login_streak', 'points')}),
+    )
+    # Restore password fields for add form
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('username', 'email', 'is_active', 'date_joined', 'login_streak', 'points')
+    exclude = ('last_login', 'is_superuser', 'groups', 'user_permissions', 'ink_bottle_returns', 'is_staff', 'password')
+
+admin.site.unregister(CustomUser) if admin.site.is_registered(CustomUser) else None
+admin.site.register(CustomUser, CustomUserAdmin)
